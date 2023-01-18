8296f431d19a0c797829f99c03f952c1c1ec58d9
danipv54 <dpvandroid54#gmail.com>
2023-01-18 14:18:14 -0400

    proxy added

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/foodly/'
+  base:'/foodly/',
+  server:{
+    cors:true,
+    proxy:{
+      'foodly': ' https://github.com/danipv54/foodly/blob/gh-pages/assets/'
+    }
+  }
   
 })
